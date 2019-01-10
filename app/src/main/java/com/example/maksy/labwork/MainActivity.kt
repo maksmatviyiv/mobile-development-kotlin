cfcfe428984bf4a69555075adccbafeb0d508b6f
maksmatviyiv <maksym.matviyiv1998@gmail.com>
2019-01-10 23:11:38 +0200

    Labwork1

---
+++ app/src/main/java/com/example/maksy/labwork/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.maksy.labwork
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println(getString(R.string.hello_world))
+    }
+}
